0d2cee46e21be6fe214b0df5e9391daa49be6294
AleksLukin <75177874+AleksLukin@users.noreply.github.com>
2020-12-21 23:16:33 +0500

    Add files via upload

---
+++ Program 3.1.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Homework3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[,] matrix = new int[2, 2];
+            matrix[1,1] = 2;
+            matrix[0, 0] = 2;
+            for (int i = 0; i < matrix.GetLength(1); i++)
+            {
+                for (int j = 0; j < matrix.GetLength(1); j++)
+                {
+                    System.Console.Write($"{matrix[i, j]} ");
+                }
+                System.Console.WriteLine();
+            }
+        }
+    }
+}
+//Написать программу, выводящую элементы двухмерного массива по диагонали
